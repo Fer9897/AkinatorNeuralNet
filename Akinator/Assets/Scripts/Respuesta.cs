800a195b5aa1bde231261532351f50735e8529c8
Fernando Espino <fer9897@gmail.com>
2019-03-26 13:07:43 -0600

    Primera carga

---
+++ Akinator/Assets/Scripts/Respuesta.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respuesta : MonoBehaviour
+{
+    public GameObject pregunta1;
+    public GameObject pregunta2;
+    public GameObject pregunta3;
+    public GameObject pregunta4;
+    public GameObject pregunta5;
+    public GameObject pregunta6;
+    public GameObject pregunta7;
+    public GameObject pregunta8;
+    public GameObject pregunta9;
+    public GameObject pregunta10;
+
+    [SerializeField]
+    private float respuesta1;
+    private float respuesta2;
+    private float respuesta3;
+    private float respuesta4;
+    private float respuesta5;
+    private float respuesta6;
+    private float respuesta7;
+    private float respuesta8;
+    private float respuesta9;
+    private float respuesta10;
+
+    public void Resp1()
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
